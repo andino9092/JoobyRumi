4d906ebb99646dfdfdefeb5ac3ab8023904d7df9
Andy <andyli9092@gmail.com>
2024-05-08 18:39:31 -0400

    Created serverUtils folder and query function

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,5 +1,38 @@
+import getQuery from "../utils/serverUtils"
 
 
-export default function Testing({}){
+export default async function Testing({}){
+
+
+    const query = `query Products {
+        products(first:100){
+          edges{
+            node{
+              title
+              handle
+              priceRange{
+                minVariantPrice{
+                  amount
+                }
+              }
+              description
+              images(first:2){
+                edges{
+                  node{
+                    url
+                    altText
+                  }
+                }
+              }
+              totalInventory
+            }
+          }
+        }
+      }`
+
+    
+
+    const products = await getQuery(query)
+
     return <div>Testing</div>
 }
---
+++ app/utils/serverUtils.tsx
@@ -0,0 +1,20 @@
+
+
+const defaultHeaders = {
+    'Content-Type': 'application/json',
+    'X-Shopify-Storefront-Access-Token': String(process.env.ACCESS_TOKEN)
+}
+
+const fetchURL = String(process.env.DOMAIN_NAME)
+
+export default async function getQuery(query: string){
+
+    
+    const res = await fetch(fetchURL, {
+        method: 'POST',
+        headers: defaultHeaders,
+        body: JSON.stringify({query}),
+    }).then((res) => res.json())
+
+    return res
+}
