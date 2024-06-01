5a1ec74fd45de1f834e729ce254d8631644d12e9
Andy Li <andyli9092@gmail.com>
2024-06-02 08:40:50 +0900

    build rerun

--- app/api/updatePrices/route.tsx
+++ app/api/updatePrices/route.tsx
@@ -1,3 +1,45 @@
 
 
 // reuse query but with a different address code. Sync with useUeffect
+
+import { defaultHeaders, fetchURL } from "@/app/utils/serverUtils";
+
+const updateAddOne = `
+  mutation updateLessOne($cartId: ID!, $lines: [CartLineUpdateInput!]!) {
+    cartLinesUpdate(cartId: $cartId, lines: $lines) {
+      cart {
+        id
+      }
+    }
+  }
+`
+
+export async function POST(request: Request){
+  const urlParts = request.url.split('?');
+  const queryParams = new URLSearchParams(urlParts.slice(1).join('?'));
+  const cartid = queryParams.get('cartid')
+  const nodeid = queryParams.get('nodeid');
+  const quantity = queryParams.get('quantity');
+  const productid = queryParams.get('productid');
+  const update = queryParams.get('update');
+  console.log(queryParams)
+
+  const res = await fetch(fetchURL, {
+      method: 'POST',
+      headers: defaultHeaders,
+      body: JSON.stringify({
+          query: updateAddOne,
+          variables: { 
+            cartId: cartid,
+            lines: [{
+              id: nodeid,
+              quantity: (Number(quantity) + Number(update)),
+              merchandiseId: productid,
+            }]
+          }
+      }),
+  })
+  const data = await res.json()
+  // console.log(data);
+  return Response.json({ data });
+}
